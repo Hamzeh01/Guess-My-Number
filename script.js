b7f1192b489b92b27e980453d656a8fc0db7eaae
Husen Hamze <summer.00.projects@gmail.com>
2023-09-20 18:23:43 +0330

    update UI/UX

--- script.js
+++ script.js
@@ -4,6 +4,7 @@
 const MIN_SCORE = 0;
 const MAX_SCORE = 20;
 
+// Find secret number between 1 and 20 (icluded)
 let randomNumber = Math.trunc(Math.random() * 20) + 1;
 let score = 20;
 let highScore = 0;
@@ -12,10 +13,12 @@ document.querySelector(".check").addEventListener("click", () => {
   const guess = +document.querySelector(".guess").value;
 
   switch (true) {
+    // CASE 1: User does not input any value
     case !guess:
       displayMessage("⛔️ No number!");
       break;
 
+    // CASE 2: User input correct guess
     case guess === randomNumber:
       displayMessage("🎉 Correct Number!");
       document.querySelector(".number").textContent = randomNumber;
@@ -28,6 +31,7 @@ document.querySelector(".check").addEventListener("click", () => {
       document.querySelector(".highscore").textContent = highScore;
       break;
 
+    // CASE 3: User cannot guess correct value
     case guess !== randomNumber:
       if (score > 0) {
         displayMessage(guess > randomNumber ? "📈 Too high!" : "📉 Too low!");
@@ -39,10 +43,24 @@ document.querySelector(".check").addEventListener("click", () => {
       }
       break;
 
+    // UNEXPECTED CASES
     default:
-        console.error(`${guess} is neither falsy value(empty) nor ${randomNumber} nor a wrong number`);
+      console.error(
+        `${guess} is neither falsy value(empty) nor ${randomNumber} nor a wrong number`
+      );
       break;
   }
+});
+
+document.querySelector(".again").addEventListener("click", () => {
+  score = MAX_SCORE;
+  randomNumber = Math.trunc(Math.random() * 20) + 1;
+  displayMessage("Start guessing...");
+  document.querySelector(".score").textContent = score;
+  document.querySelector(".number").textContent = "?";
+  document.querySelector(".guess").value = "";
+  document.querySelector(".number").style.width = "15rem";
+  document.querySelector("body").style.backgroundColor = "#222";
 });
 
 const displayMessage = function (message) {
